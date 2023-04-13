84e30834477263c36f2aa07a0981ddef666389f3
Teddy Byron <ted@tedbyron.com>
2023-04-13 00:04:15 -0400

    fix errors

--- src/lib.rs
+++ src/lib.rs
@@ -3,42 +3,55 @@
 #![feature(lazy_cell)]
 #![doc = include_str!("../README.md")]
 
-use std::borrow::Cow;
-use std::cell::LazyCell;
-
-use embedded_svc::mqtt::client::{Details, Event, MessageId, QoS};
+use embedded_svc::mqtt::client::{Event, MessageId, QoS};
 use esp_idf_svc::mqtt::client::{EspMqttClient, EspMqttMessage, MqttClientConfiguration};
 use esp_idf_sys::EspError;
 
-mod config;
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    pub username: &'static str,
+    #[default("")]
+    pub password: &'static str,
+}
 
-pub const BROKER_URL: LazyCell<Cow<str>> = LazyCell::new(|| {
-    let MqttConfig { username, password } = CONFIG.mqtt;
-    if username.is_empty() {
-        Cow::Borrowed("mqtt://broker.losant.com")
-    } else {
-        Cow::Owned(format!("mqtt://{username}:{password}@broker.losant.com"))
-    }
-});
+pub type Result<T> = std::result::Result<T, EspError>;
 
 pub struct Device<'a> {
-    pub mqtt_config: MqttClientConfiguration,
+    pub mqtt_config: MqttClientConfiguration<'a>,
     pub mqtt_client: EspMqttClient,
 }
 
 impl<'a> Device<'a> {
-    pub fn new() -> Self {
-        Self {
-            mqtt_config: MqttClientConfiguration::default(),
-            mqtt_client: EspMqttClient::new_with_conn(*BROKER_URL, &mqtt_config),
+    fn broker_url() -> String {
+        let Config { username, password } = CONFIG;
+        if username.is_empty() || password.is_empty() {
+            panic!("username and password must be set in cfg.toml");
+        } else {
+            format!("mqtt://{username}:{password}@broker.losant.com")
         }
     }
 
-    pub fn with_conn(config: &'a MqttClientConfiguration<'a>) -> Self {
-        Self {
-            mqtt_config: MqttClientConfiguration::default(),
-            mqtt_client: EspMqttClient::new_with_conn(*BROKER_URL, config),
-        }
+    pub fn new<F>(
+        callback: impl for<'b> FnMut(&'b Result<Event<EspMqttMessage<'b>>>) + Send + 'static,
+    ) -> Result<Self> {
+        let mqtt_config = MqttClientConfiguration::default();
+        let mqtt_client = EspMqttClient::new(Self::broker_url(), &mqtt_config, callback)?;
+        Ok(Self {
+            mqtt_config,
+            mqtt_client,
+        })
+    }
+
+    pub fn with_config(
+        config: MqttClientConfiguration<'a>,
+        callback: impl for<'b> FnMut(&'b Result<Event<EspMqttMessage<'b>>>) + Send + 'static,
+    ) -> Result<Self> {
+        let mqtt_client = EspMqttClient::new(Self::broker_url(), &config, callback)?;
+        Ok(Self {
+            mqtt_config: config,
+            mqtt_client,
+        })
     }
 
     pub fn enqueue(
@@ -47,7 +60,7 @@ impl<'a> Device<'a> {
         qos: QoS,
         retain: bool,
         payload: &[u8],
-    ) -> Result<MessageId, EspError> {
+    ) -> Result<MessageId> {
         self.mqtt_client
             .enqueue(topic.as_ref(), qos, retain, payload)
     }
@@ -58,12 +71,8 @@ impl<'a> Device<'a> {
         qos: QoS,
         retain: bool,
         payload: &[u8],
-    ) -> Result<MessageId, EspError> {
+    ) -> Result<MessageId> {
         self.mqtt_client
             .publish(topic.as_ref(), qos, retain, payload)
     }
-}
-
-pub fn topic_builder(uuid: impl AsRef<str>) -> impl Fn(impl AsRef<str>) -> String {
-    move |topic| format!("{}/{}", uuid.as_ref(), topic)
 }
